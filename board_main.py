da06d7ac639e6ac2f59bd31402443e5e4541d849
Jonah <jonah_lefkoff@hotmail.com>
2021-03-19 10:03:42 -0600

    added button press to exit logic

--- board_main.py
+++ board_main.py
@@ -1,6 +1,7 @@
 ## This is the main program running the gridboard ##
 
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
+from PIL import Image
 
 #import ticTacToeGame #games
 
@@ -30,9 +31,15 @@ options.pixel_mapper_config = 'U-mapper'
 
 matrix = RGBMatrix(options = options) #making the matrix for all programs
 
+blankscreen = Image.new('RGB', (128, 128))
+
+def drawBlank():
+    matrix.SetImage(blankscreen,0,0)
+
 if __name__ == '__main__':
-	while True:
+        print("initalized")
+	while start_screen.mainPress == False:
 	    start_screen.mainScreen(matrix)
-    	    print("initalized")
 
 
+            
--- start_screen.py
+++ start_screen.py
@@ -1,5 +1,21 @@
 from PIL import Image, ImageDraw, ImageFont
 
+import get_buttons
+
 def mainScreen(rgbmatrix):
     mainscreen = Image.open("converted-gifs/Top/Homescreen/Homescreen.gif02.gif").convert("RGB")
     rgbmatrix.SetImage(mainscreen,0,0)
+
+def mainPress():
+    my_button = get_buttons.read()
+    if my_button != None:
+      print("Main - button x: "+str(my_button[0]))
+      print("Main - button y: "+str(my_button[1]))
+  
+      x = my_button[0] * 32
+      y = my_button[1] * 32
+  
+      if my_button[2]=="P":
+        return True
+      else:
+        return False
