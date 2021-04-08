55cb07c25db83c56494a01c8ad1515986e2cf7d8
gsalaman <g.salaman@comcast.net>
2021-04-08 20:18:40 +0100

    fix array definition

--- get_button_dev/button_test_block.py
+++ get_button_dev/button_test_block.py
@@ -10,21 +10,21 @@ from rgbmatrix import RGBMatrix, RGBMatrixOptions
 from PIL import Image, ImageDraw, ImageFont
 
 # this is the size of ONE of our matrixes. 
-#matrix_rows = 32 
-#matrix_columns = 32 
-matrix_rows = 64 
-matrix_columns = 64 
+matrix_rows = 32 
+matrix_columns = 32 
+#matrix_rows = 64 
+#matrix_columns = 64 
 
 # how many matrixes stacked horizontally and vertically 
-#matrix_horizontal = 8 
-#matrix_vertical = 2
-matrix_horizontal = 1 
-matrix_vertical = 1
+matrix_horizontal = 8 
+matrix_vertical = 2
+#matrix_horizontal = 1 
+#matrix_vertical = 1
 
-#total_rows = 128 
-#total_columns = 128
-total_rows = 64 
-total_columns = 64 
+total_rows = 128 
+total_columns = 128
+#total_rows = 64 
+#total_columns = 64 
 
 options = RGBMatrixOptions()
 options.rows = matrix_rows 
@@ -32,7 +32,7 @@ options.cols = matrix_columns
 options.chain_length = matrix_horizontal
 options.parallel = matrix_vertical 
 options.hardware_mapping = 'regular' 
-#options.pixel_mapper_config = 'U-mapper'
+options.pixel_mapper_config = 'U-mapper'
 options.gpio_slowdown = 2
 
 matrix = RGBMatrix(options = options)
@@ -48,7 +48,7 @@ blank = Image.new("RGB", (16,16))
 # This version only does presses...and toggles the ghost image on and off 
 # for each press.  This means we have to remember, for our 8x8 grid, whether
 # the square in question is on or off
-grid = [[0]*8]*8
+grid = [[0 for i in range(8)] for j in range(8)]
 
 print grid
 
@@ -69,6 +69,8 @@ try:
     else:
           matrix.SetImage(blank,x*16,y*16)
           grid[x][y] = 0
+
+    print grid
 
 except KeyboardInterrupt:
   exit(0)
